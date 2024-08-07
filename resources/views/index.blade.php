6c4b3899523315d14f26f04f5a5c65efdae128d8
Rodless <Trustful69@gmail.com>
2024-08-07 07:06:22 +0530

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Suspects</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <h3>Suspects all data</h3>
+    <input type="text" name="suspect_filter" id="suspect">
+    <div id="output"></div>
+</body>
+</html>
